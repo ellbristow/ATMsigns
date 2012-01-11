70a58d43032e5496d414588e10c5675eb7b1bf53
Elliott Bristow <admin@jigsawspain.com>
2012-01-11 12:46:50 +0100

    0.0.1 beta First release

---
+++ src/me/ellbristow/ATMsigns/playerListener.java
@@ -0,0 +1,86 @@
+package me.ellbristow.ATMsigns;
+
+import java.util.logging.Logger;
+
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerListener;
+
+public class playerListener extends PlayerListener {
+
+	public static ATMsigns plugin;
+	public static Economy economy;
+	
+	public final Logger logger = Logger.getLogger("Minecraft");
+	
+	public playerListener (ATMsigns instance, Economy eco) {
+		economy = eco;
+	}
+	
+	public void onPlayerInteract (PlayerInteractEvent event) {
+		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && (event.getClickedBlock().getType().name().equals("SIGN_POST") || event.getClickedBlock().getType().name().equals("WALL_SIGN"))) {
+			Block block = event.getClickedBlock();
+			Sign sign = (Sign) block.getState();
+			if (sign.getLine(0).equals(ChatColor.WHITE + "=== ATM ===")) {
+				// Player right-clicked an ATM sign
+				Player player = event.getPlayer();
+				if (player.hasPermission("atmsigns.use")) {
+					if (sign.getLine(1).startsWith(ChatColor.GREEN + ">")) {
+						// DEPOSITING
+						ATMsigns.depositItem(player);
+					}
+					else if (sign.getLine(2).startsWith(ChatColor.GREEN + ">")) {
+						// WITHDRAWING
+						ATMsigns.withdrawItem(player);
+					}
+					else if (sign.getLine(3).startsWith(ChatColor.GREEN + ">")) {
+						// BALANCE
+						ATMsigns.getBalance(player);
+					}
+				}
+				else {
+					player.sendMessage(ChatColor.RED + "You do not have permission to use ATM signs!");
+				}
+				event.setCancelled(true);
+			}
+		}
+		else if (event.getAction().equals(Action.LEFT_CLICK_BLOCK) && (event.getClickedBlock().getType().name().equals("SIGN_POST") || event.getClickedBlock().getType().name().equals("WALL_SIGN"))) {
+			Block block = event.getClickedBlock();
+			Sign sign = (Sign) block.getState();
+			Player player = event.getPlayer();
+			if (sign.getLine(0).equals(ChatColor.WHITE + "=== ATM ===")) {
+				// Player left-clicked an ATM sign
+				if (player.hasPermission("atmsigns.use")) {
+					if (sign.getLine(1).startsWith(ChatColor.GREEN + ">")) {
+						// Deposit, switch to Withdraw
+						sign.setLine(1, "  Deposit      ");
+						sign.setLine(2, ChatColor.GREEN + ">" + ChatColor.BLACK + " Withdraw     ");
+						sign.update();
+					}
+					else if (sign.getLine(2).startsWith(ChatColor.GREEN + ">")) {
+						// Withdraw, switch to Balance
+						sign.setLine(2, "  Withdraw     ");
+						sign.setLine(3, ChatColor.GREEN + ">" + ChatColor.BLACK + " Balance      ");
+						sign.update();
+					}
+					else if (sign.getLine(3).startsWith(ChatColor.GREEN + ">")) {
+						// Balance, switch to Deposit
+						sign.setLine(3, "  Balance      ");
+						sign.setLine(1, ChatColor.GREEN + ">" + ChatColor.BLACK + " Deposit      ");
+						sign.update();
+					}
+				}
+				else {
+					player.sendMessage(ChatColor.RED + "You do not have permission to use ATM signs!");
+					event.setCancelled(true);
+				}
+			}
+		}
+	}
+}
---
+++ src/me/ellbristow/ATMsigns/signListener.java
@@ -0,0 +1,53 @@
+package me.ellbristow.ATMsigns;
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.SignChangeEvent;
+
+public class signListener extends BlockListener {
+	
+	public static ATMsigns plugin;
+	
+	public final Logger logger = Logger.getLogger("Minecraft");
+	
+	public signListener (ATMsigns instance) {
+		
+	}
+	
+	public void onSignChange (SignChangeEvent event) {
+		SignChangeEvent sign = event;
+		Player player = event.getPlayer();
+		if ((sign.getLine(0).equalsIgnoreCase(ChatColor.WHITE + "=== ATM ==="))) {
+			// Player trying to manually create sign (possible permission bypass)
+			player.sendMessage(ChatColor.RED + "Please use [atm] to make ATM signs!");
+			event.setCancelled(true);
+			return;
+		}
+		if (sign.getLine(0).equalsIgnoreCase("[atm]") && (player.hasPermission("atmsigns.create"))) {
+			sign.setLine(0, ChatColor.WHITE + "=== ATM ===");
+			sign.setLine(1, ChatColor.GREEN + ">" + ChatColor.BLACK + " Deposit      ");
+			sign.setLine(2, "  Withdraw     ");
+			sign.setLine(3, "  Balance      ");
+		}
+	}
+	
+	public void onBlockBreak (BlockBreakEvent event) {
+		Block block = event.getBlock();
+		if (block.getTypeId() == 63 || block.getTypeId() == 68) {
+			Player player = event.getPlayer();
+			Sign sign = (Sign) block.getState();
+			if (sign.getLine(1) == ChatColor.WHITE + "=== ATM ===" && !player.hasPermission("atmsigns.destroy")) {
+				player.sendMessage(ChatColor.RED + "You do not have permission to break ATM signs!");
+				event.setCancelled(true);
+				sign.setLine(1, sign.getLine(1));
+				sign.update();
+			 }
+		}
+	}
+}
